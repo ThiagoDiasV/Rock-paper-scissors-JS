fd36bd99abae9ea901d30a68ff406a27618a53e5
ThiagoDiasV <thiago76ers@gmail.com>
2020-02-05 23:16:25 -0300

    refactoring JavaScript

--- main.js
+++ main.js
@@ -1,59 +1,81 @@
-const pedraJogada = document.getElementById("pedra");
-const papelJogada = document.getElementById("papel");
-const tesouraJogada = document.getElementById("tesoura");
-const pontuacaoJogador = document.getElementById("jogador-pontos");
-const pontuacaoComputador = document.getElementById("computador-pontos");
-const mensagemResultado = document.getElementById("mensagem-resultado");
+const pedraJogada = document.getElementById('pedra');
+const papelJogada = document.getElementById('papel');
+const tesouraJogada = document.getElementById('tesoura');
+const pontuacaoJogador = document.getElementById('jogador-pontos');
+const pontuacaoComputador = document.getElementById('computador-pontos');
+const mensagemResultado = document.getElementById('mensagem-resultado');
 
 let pontosJogador = parseInt(pontuacaoJogador.innerText);
 let pontosComputador = parseInt(pontuacaoComputador.innerText);
 
 function computadorFazJogada() {
-  possibilidades = ["Pedra", "Papel", "Tesoura"];
+  possibilidades = ['Pedra', 'Papel', 'Tesoura'];
   jogadaComputador =
     possibilidades[Math.floor(Math.random() * possibilidades.length)];
   return jogadaComputador;
 }
 
-function jogadorVence() {}
+function jogadorVence(jogadaUsuario, jogadaComputador) {
+  pontosJogador++;
+  pontuacaoJogador.innerText = pontosJogador;
+  let resultadoMensagem = `${jogadaUsuario} ganha de ${jogadaComputador}! Você venceu!`;
+  mensagemResultado.innerText = resultadoMensagem;
+  let vencedor = 'Jogador';
+  return vencedor;
+}
+
+function computadorVence(jogadaUsuario, jogadaComputador) {
+  pontosComputador++;
+  pontuacaoComputador.innerText = pontosComputador;
+  let resultadoMensagem = `${jogadaComputador} ganha de ${jogadaUsuario}! O computador venceu!`;
+  mensagemResultado.innerText = resultadoMensagem;
+  let vencedor = 'Computador';
+  return vencedor;
+}
+
+function empate(jogadaUsuario, jogadaComputador) {
+  let resultadoMensagem = `${jogadaUsuario} é igual a ${jogadaComputador}. Empate!`;
+  mensagemResultado.innerText = resultadoMensagem;
+  return 'Empate';
+}
 
 function avaliaJogada(jogadaUsuario, jogadaComputador) {
   switch (jogadaUsuario + jogadaComputador) {
-    case "PedraTesoura":
-    case "PapelPedra":
-    case "TesouraPapel":
-      console.log("Jogador venceu!");
+    case 'PedraTesoura':
+    case 'PapelPedra':
+    case 'TesouraPapel':
+      jogadorVence(jogadaUsuario, jogadaComputador);
       break;
-    case "PedraPapel":
-    case "PapelTesoura":
-    case "TesouraPedra":
-      console.log("Computador venceu!");
+    case 'PedraPapel':
+    case 'PapelTesoura':
+    case 'TesouraPedra':
+      computadorVence(jogadaUsuario, jogadaComputador);
       break;
-    case "PedraPedra":
-    case "PapelPapel":
-    case "TesouraTesoura":
-      console.log("Empate!");
+    case 'PedraPedra':
+    case 'PapelPapel':
+    case 'TesouraTesoura':
+      empate(jogadaUsuario, jogadaComputador);
       break;
   }
 }
 
 function main() {
-  pedraJogada.addEventListener("click", function() {
-    jogadaUsuario = "Pedra";
+  pedraJogada.addEventListener('click', function() {
+    jogadaUsuario = 'Pedra';
     jogadaComputador = computadorFazJogada();
-    avaliaJogada(jogadaUsuario, jogadaComputador);
+    let resultadoVencedor = avaliaJogada(jogadaUsuario, jogadaComputador);
   });
 
-  papelJogada.addEventListener("click", function() {
-    jogadaUsuario = "Papel";
+  papelJogada.addEventListener('click', function() {
+    jogadaUsuario = 'Papel';
     jogadaComputador = computadorFazJogada();
-    avaliaJogada(jogadaUsuario, jogadaComputador);
+    let resultadoVencedor = avaliaJogada(jogadaUsuario, jogadaComputador);
   });
 
-  tesouraJogada.addEventListener("click", function() {
-    jogadaUsuario = "Tesoura";
+  tesouraJogada.addEventListener('click', function() {
+    jogadaUsuario = 'Tesoura';
     jogadaComputador = computadorFazJogada();
-    avaliaJogada(jogadaUsuario, jogadaComputador);
+    let resultadoVencedor = avaliaJogada(jogadaUsuario, jogadaComputador);
   });
 }
 
